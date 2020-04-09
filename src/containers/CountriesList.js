72401859ef8eb4209a0378da64e8c0e9a4bada72
Jimoh Abdulrahman <39130903+abdol4real@users.noreply.github.com>
2020-04-09 22:10:38 +0100

    Search functionality

--- src/containers/CountriesList.js
+++ src/containers/CountriesList.js
@@ -4,33 +4,42 @@ import { Country } from './Country';
 
 export const CountriesList = () => {
     const [countries, setcountries] = useState([]);
-    const [isLoading, setLoading] = useState(true)
+    const [isLoading, setLoading] = useState(true);
+    const [search, setSearch] = useState("");
+    const [isOpen, setIsOpen] = useState(false)
+
     useEffect(() => {
         const fetchData = async () => {
             const response = await fetch("https://restcountries.eu/rest/v2/all") 
             const data = await response.json();
-            setcountries(data)
+            setcountries(data);
+            setLoading(false) ;
         }  
         fetchData(); 
-        setLoading(false) 
-    }, [])
+        
+    }, [countries, isOpen]);
 
-    const items = countries.map(item => {
+    const handleSearch = e => {
+        setSearch(e.target.value);
+        console.log(search)
+    }
+
+    let items = countries.filter(item => {return item.name.toLowerCase().indexOf(search) !== -1}).map(item => {
         return (<Country key={item.id} country={item}/>)
-    })
+    });
 
-    let display = isLoading ? "loading..." : items
+    let display = isLoading ? "loading..." : items;
     return (
         <Auxiliary>
             <section className="container">
                 <div className="row">
                     <div className="ten columns"> 
-                        <input className="search" id="search" type="text" placeholder="Search for a country" name="" value="" />
+                        <input className="search" id="search" type="text" placeholder="Search for a country" name="search" value={search} onChange={handleSearch}/>
                     </div>
-                    <div className="two columns dropdown" id="dropdown">
+                    <div className="two columns dropdown" id="dropdown" onClick={()=>{setIsOpen(!isOpen)}}>
                         <p>Filter by Region</p>
 					<i className="fas fa-chevron-down" class="region"></i>
-                        <ul>
+                        <ul className={isOpen ? "open" : "hide"}>
                             <li>Africa</li>
                             <li>America</li>
                             <li>Asia</li>
